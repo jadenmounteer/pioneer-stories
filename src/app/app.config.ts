6b02fe2f0da9edcd71f62a6648b4aa5344643ccf
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-24 20:25:33 -0600

    Initialize firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => {
+      return getFirestore();
+    }),
+    provideAnimationsAsync(),
+  ],
 };

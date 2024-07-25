c30b9b965150bb2114a777a55ea104f59b52b2df
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-25 14:40:42 -0600

    Begin creating story service

--- src/app/stories/services/story.service.ts
+++ src/app/stories/services/story.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Story } from '../types/story';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StoryService {
+  constructor(private firestore: Firestore) {}
 
-  constructor() { }
+  public getStory(storyID: string): Observable<Story[]> {
+    const collectionRef = collection(this.firestore, 'stories');
+
+    return collectionData(collectionRef, {}) as Observable<Story[]>;
+  }
 }

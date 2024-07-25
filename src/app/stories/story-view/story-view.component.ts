d1f8ca631f76196719f603ca8fcdfe2563deac5f
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-25 16:12:12 -0600

    Get story signal

--- src/app/stories/story-view/story-view.component.ts
+++ src/app/stories/story-view/story-view.component.ts
@@ -1,21 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { StoryService } from '../services/story.service';
+import { Story } from '../types/story';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-story-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './story-view.component.html',
   styleUrl: './story-view.component.scss',
 })
 export class StoryViewComponent {
   storyService: StoryService = inject(StoryService);
+  story: Signal<Story | undefined> = toSignal(
+    this.storyService.getStory('tFc2ePZtmLIdy0T5gUz5')
+  );
 
   constructor() {}
 
-  ngOnInit() {
-    this.storyService.getStory('tFc2ePZtmLIdy0T5gUz5').subscribe((data) => {
-      console.log(data);
-    });
-  }
+  ngOnInit() {}
 }

688303fc5e47788abd667f0eb00286536c1fcbce
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-26 16:44:24 -0600

    Grab the audioFileUrl

--- src/app/stories/audio-player/audio-player/audio-player.component.ts
+++ src/app/stories/audio-player/audio-player/audio-player.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, InputSignal } from '@angular/core';
+import { FileService } from '../../../files/services/file.service';
 
 @Component({
   selector: 'app-audio-player',
   standalone: true,
   imports: [],
   templateUrl: './audio-player.component.html',
-  styleUrl: './audio-player.component.scss'
+  styleUrl: './audio-player.component.scss',
 })
 export class AudioPlayerComponent {
+  private fileService: FileService = inject(FileService);
 
+  public audioFilePath: InputSignal<string> = input.required();
+
+  protected audioUrl = computed(async () => {
+    const url = await this.fileService.getFileUrl(this.audioFilePath());
+    return url;
+  });
 }
